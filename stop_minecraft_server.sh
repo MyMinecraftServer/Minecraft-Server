0480469d297588e81576118bacdb9c771fee5f6c
MyMinecraftServer <64504397+MyMinecraftServer@users.noreply.github.com>
2021-01-08 23:15:01 -0600

    script now stops the backup script too
    made it more convenient to stop all the processes related to the server

--- stop_minecraft_server.sh
+++ stop_minecraft_server.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 # send the stop command to minecraft server
 screen -p 0 -S minecraft -X eval 'stuff "stop"\015'
+# wait for it actually stop
 sleep 10s
+./back_up_scripts/stop_backups.sh
+# guess that the server stopped and lie to the user that it is (whem maybe it is not and you should wait longer)
 echo "stopped server"
